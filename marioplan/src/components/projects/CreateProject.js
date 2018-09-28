8b7ea1bd434c8996d5159da78b4f0de8f9b12336
Mario Lazzari <mario.lazzari@gmail.com>
2018-09-28 11:44:19 +0200

    Done lesson 14

--- marioplan/src/components/projects/CreateProject.js
+++ marioplan/src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
 
 class CreateProject extends Component {
   state = {
@@ -12,7 +14,7 @@ class CreateProject extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.createProject(this.state);
   };
 
   render() {
@@ -32,7 +34,7 @@ class CreateProject extends Component {
               onChange={this.handleChange}
             />
             <div className="input-field">
-              <button className="btn pink lighten-1 z-depth-0">Login</button>
+              <button className="btn pink lighten-1 z-depth-0">Create</button>
             </div>
           </div>
         </form>
@@ -41,4 +43,13 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject;
+const mapDispatchToProps = dispatch => {
+  return {
+    createProject: project => dispatch(createProject(project))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateProject);
--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -4,12 +4,13 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 // redux
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 // create redux store and associate root reducer
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
